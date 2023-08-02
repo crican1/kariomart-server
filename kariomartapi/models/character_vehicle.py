99b7a5435fb8a2e5f38f154428f825563077b116
Tyler Eisen <jteisen90@gmail.com>
2023-08-02 07:01:50 +0700

    CRUD character, vehicle, map, character_vehicle entities

--- kariomartapi/models/character_vehicle.py
+++ kariomartapi/models/character_vehicle.py
@@ -5,4 +5,5 @@ from .vehicle import Vehicle
 class CharacterVehicle(models.Model):
 
     character_id = models.ForeignKey(Character, on_delete=models.CASCADE)
+    
     vehicle_id = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
