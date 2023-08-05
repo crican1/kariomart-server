0bd81e52a88557b8ef4a4aa150cd4805903f2516
Tyler Eisen <jteisen90@gmail.com>
2023-08-05 23:24:30 +0700

    user model, view, auth view, check_user and register_user decorators added

--- kariomart/urls.py
+++ kariomart/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from kariomartapi.views import CharacterView, VehicleView, MapView, CharacterVehicleView, RaceView
+from kariomartapi.views import CharacterView, VehicleView, MapView, CharacterVehicleView, RaceView, UserView
+from kariomartapi.views.auth import register_user, check_user
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'characters', CharacterView, 'character')
@@ -9,8 +10,11 @@ router.register(r'vehicles', VehicleView, 'vehicle')
 router.register(r'maps', MapView, 'map')
 router.register(r'character_vehicles', CharacterVehicleView, 'character_vehicle')
 router.register(r'races', RaceView, 'race')
+router.register(r'users', UserView, 'user')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
      path('', include(router.urls)),
+     path('register', register_user),
+    path('checkuser', check_user),
 ]
--- kariomartapi/models/__init__.py
+++ kariomartapi/models/__init__.py
@@ -3,3 +3,4 @@ from .vehicle import Vehicle
 from .character_vehicle import CharacterVehicle
 from .map import Map
 from .race import Race
+from .user import User
