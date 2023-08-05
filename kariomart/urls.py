560f558fceb1c0abb7d45e95e6d3bb38a1aee603
Tyler Eisen <jteisen90@gmail.com>
2023-08-06 01:08:43 +0700

    race model and view updated, chracter_vehicle removed

--- kariomart/urls.py
+++ kariomart/urls.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from kariomartapi.views import CharacterView, VehicleView, MapView, CharacterVehicleView, RaceView, UserView
+from kariomartapi.views import CharacterView, VehicleView, MapView, RaceView, UserView
 from kariomartapi.views.auth import register_user, check_user
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'characters', CharacterView, 'character')
 router.register(r'vehicles', VehicleView, 'vehicle')
 router.register(r'maps', MapView, 'map')
-router.register(r'character_vehicles', CharacterVehicleView, 'character_vehicle')
 router.register(r'races', RaceView, 'race')
 router.register(r'users', UserView, 'user')
 
--- kariomartapi/models/character_vehicle.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from .character import Character
-from .vehicle import Vehicle
-
-class CharacterVehicle(models.Model):
-
-    character_id = models.ForeignKey(Character, on_delete=models.CASCADE)
-    
-    vehicle_id = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
